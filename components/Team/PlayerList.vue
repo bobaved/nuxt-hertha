d6ae3d1e14ddff4e9c3b6efca1c9ea23f23882e2
Leonard Kriese <leonard.kriese@turbinekreuzberg.com>
2021-12-25 22:27:42 +0100

    add player list to teams page

---
+++ components/Team/PlayerList.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col
+        v-for="player in players"
+        :key="player.id"
+        col=4
+        md=4
+        sm=12
+      >
+        <v-hover>
+          <template v-slot:default="{ hover }">
+            <v-card>
+              <v-img
+                src="https://placeimg.com/640/480/sport/sepia"
+              >
+              </v-img>
+              <v-card-title>
+                {{player.attributes.Name}} {{player.attributes.LastName}}
+                
+              </v-card-title>
+
+              <v-expand-transition>
+                <v-card
+                  v-if="hover"
+                  class="transition-fast-in-fast-out v-card--reveal"
+                  style="height: 100%;"
+                >
+                  <v-card-title>
+                    {{player.attributes.Name}} {{player.attributes.LastName}}
+                  </v-card-title>
+                  <v-card-text>
+                    Nochn bissl Fließtext
+                  </v-card-text>
+                  <v-list one-line>
+                    <v-list-item>
+                      <v-list-item-content>
+                        <v-btn
+                          color="blue-grey"
+                          class="white--text"
+                        >
+                          QTTR Profil
+                        <v-icon right color="white">
+                          mdi-account
+                        </v-icon>
+                        </v-btn>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-card>
+              </v-expand-transition>
+            </v-card>
+          </template>
+        </v-hover>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+import { Player } from '~/types/types'
+
+@Component({
+  name: 'PlayerList',
+
+})
+export default class PlayerList extends Vue {
+  @Prop({required: true, type: Array as () => Player[] })
+  players!: Player[]
+}
+</script>
+<style>
+.v-card--reveal {
+  bottom: 0;
+  opacity: 0.9 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
--- pages/teams.vue
+++ pages/teams.vue
@@ -1,25 +1,48 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      {{ teams }}
-      <ul>
-        <li v-for="team in teams" :key="team.attributes.id">
-          {{ team.attributes}}
-        </li>
-      </ul>
-    </v-col>
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12" sm="8" md="12">
+      <v-card>
+        <v-toolbar
+          flat
+          color="primary"
+          dark
+        >
+          <v-toolbar-title>Unsere Mannschaften</v-toolbar-title>
+        </v-toolbar>
+        <v-tabs vertical>
+          <v-tab
+            v-for="team in teams"
+            :key="team.attributes.id"
+          >
+            {{ team.attributes.Titel}}
+          </v-tab>
+          <v-tab-item
+            v-for="team in teams"
+            :key="team.attributes.id"
+          >
+            <v-card flat>
+              <v-card-text>
+                Hier könnte noch eine kleine Beschreibung der Mannschaft stehen. Vllt die Tabelle
+              </v-card-text>              
+              <player-list v-if="team.attributes.players.data" :players="team.attributes.players.data"/>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+      </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import gql from 'graphql-tag'
 import { NuxtContext } from '~/interfaces/context'
 import { Team } from '~/types/types'
 import teamsQuery from '~/apollo/queries/teams.gql'
+import PlayerList from '~/components/Team/PlayerList.vue'
 export default Vue.extend({
+  components: {
+    PlayerList
+  },
 
   async asyncData (ctx: NuxtContext): Promise<{teams: Team[]}> {
     const result = await ctx.app.apolloProvider.defaultClient.query({
@@ -28,10 +51,5 @@ export default Vue.extend({
     console.log(result.data.teams.data)
     return { teams: result.data.teams.data }
   },
-  data () {
-    return {
-      teams: null
-    }
-  }
 })
 </script>
